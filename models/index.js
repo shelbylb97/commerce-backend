85436eedca145a9f0f8942a2ed3bb4ab7abbdcaa
Shelby Butt <shelbybutt@shelbys-mbp.lan>
2021-08-30 18:30:21 -0400

    working with models

--- models/index.js
+++ models/index.js
@@ -5,13 +5,30 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, 
+  { foreignKey: 'category_id', onDelete: 'cascade', hooks:true});
 
 // Categories have many Products
+Category.hasMany(Product, 
+  { onDelete: 'Set Null', hooks:true });
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: {
+    model: 'product_tag',
+    unique: false,
+  },
+  foreignKey: 'tag_id'
+});
 module.exports = {
   Product,
   Category,
